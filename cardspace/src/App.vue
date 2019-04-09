a55a7ce6f405d9082f97f43f5f49f36efd8f228d
John Preston <wcerfgba@riseup.net>
2019-04-09 13:10:21 +0100

    Basic vue app with cards opening

---
+++ cardspace/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app" v-on:click="closeCards">
+    <Card
+      title="yaboi"
+      icon="favicon.ico"
+      ref="card1"
+    >
+      <p>yeeeah</p>
+    </Card>
+    <Card
+      title="yaboi"
+      icon="favicon.ico"
+      ref="card2"
+    >
+      <p>yeeeah</p>
+    </Card>
+    <Card
+      title="yaboi"
+      icon="favicon.ico"
+      ref="card3"
+    >
+      <p>yeeeah</p>
+    </Card>
+  </div>
+</template>
+
+<script>
+import { values, filter, map } from 'ramda'
+import Card from './components/Card.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Card
+  },
+  data: function () {
+    return {
+    }
+  },
+  methods: {
+    cards: function () {
+      return values(filter(ref => ref.$options._componentTag === 'Card', this.$refs || []))
+    },
+    closeCards: function () {
+      map(card => card.close(), this.cards())
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cardspace/src/components/Card.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card">
+    <div class="far-away">
+      <Floating class="far-away-floater">
+        <img v-bind:src="icon" v-bind:alt="title" v-on:click.capture.stop="open">
+      </Floating>
+    </div>
+    <div class="close-up" v-show="isOpen">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Floating from './Floating.vue'
+
+export default {
+  data: function () {
+    return {
+      isOpen: false
+    }
+  },
+  props: {
+    icon: String,
+    title: String
+  },
+  methods: {
+    open: function () {
+      this.isOpen = true;
+    },
+    close: function () {
+      this.isOpen = false;
+    }
+  },
+  components: {
+    Floating
+  }
+}
+</script>
+
+<style>
+.far-away {
+  display: flex;
+}
+
+.far-away-floater {
+  display: flex;
+  cursor: pointer;
+}
+
+.close-up {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #f00;
+  margin: 3rem;
+}
+</style>
