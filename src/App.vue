04d4673b169f4387f7ee8a69d9d99255e8fe490e
John Preston <wcerfgba@riseup.net>
2019-04-29 21:10:03 +0100

    improoove

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="app">
     <div class="slice">
       <div class="big-red">
-        <p>I am a <span>multimedia artist</span> based in the <span>North West, United Kingdom</span>. My background is in computer science and software engineering. I am primarily interested in works which raise questions about <span>sociotechnical systems</span>, and our relationships with other people, ourselves, and the <span>natural world</span>.</p>
+        <p>I am a <span>multimedia artist</span> based in the <span>North West, United Kingdom</span>. My background is in computer science and software engineering. I am primarily interested in works which raise questions about <span>sociotechnical systems</span>, and our relationships with ourselves, other people, and the <span>natural world</span>.</p>
         <p>Thanks for visiting.</p>
       </div>
     </div>
@@ -11,20 +11,22 @@
         <i-can-feel ref="icanfeel"></i-can-feel>
       </div>
       <div class="right">
-        <h2>I CAN FEEL</h2>
-        <span class="date">22222</span>
-        <p>
-          The piece is composed of a 13x13 grid of squares, each of which changes between black and white at a set rate. The frequency of each oscillator is chosen such that the grid will eventually produce every 13x13 black and white picture, before looping. This takes approximately 10<span class="superscript">409</span> years.
-        </p>
-        <p>
-          The initial state of the oscillators has been chosen such that after approximately 60 seconds, the eponymous phrase <em>I CAN FEEL</em> appears in the image. <span class="action" @click="resetICanFeel">Click here</span> to reset the simulation to the beginning.
-        </p>
-        <p>
-          simple algorithm, meaningless data, but embodies meaning to humans.
-            what delineates our own sentience?
-            how would we know if a machine is sentient?
-          
-        </p>
+        <div class="right-inner">
+          <h2>I CAN FEEL</h2>
+          <span class="date">2018</span>
+          <p>
+            The piece is composed of a 13x13 grid of squares, each of which changes between black and white at a set rate. The frequency of each oscillator is chosen such that the grid will eventually produce every 13x13 black and white picture, before looping. This takes approximately 10<span class="superscript">409</span> years.
+          </p>
+          <p>
+            The initial state of the oscillators has been chosen such that after approximately 60 seconds, the eponymous phrase <em>I CAN FEEL</em> appears in the image. <span class="action" @click="resetICanFeel">Click here</span> to reset the simulation to the beginning.
+          </p>
+          <p>
+            simple algorithm, meaningless data, but embodies meaning to humans.
+              what delineates our own sentience?
+              how would we know if a machine is sentient?
+            
+          </p>
+        </div>
       </div>
     </div>
 
@@ -33,14 +35,15 @@
         <gradient-resonance></gradient-resonance>
       </div>
       <div class="right">
-        <h2>Gradient Resonance</h2>
-        <span class="date">2019</span>
-        <p>This piece is interactive, please move your cursor over or tap on the canvas.</p>
-        <p>Inspired by Bridget Riley's op art and the wallpaper in the confessional of recent seasons of RuPaul's Drag Race, <em>Gradient Resonance</em> is an interactive op art piece which allows an audience member to create a linear gradient from any two full-saturation colors.</p>
-        <p>The resulting gradient is re-imposed on top of itself in a pattern. This creates an effect of different squares fading in from the top and bottom of the piece. The prominence of the effect will vary depending on the colours chosen for the gradient, and some interesting combinations have been highlighted on the piece.</p>
-        <p>
-          themes???
-        </p>
+        <div class="right-inner">
+          <h2>Gradient Resonance</h2>
+          <span class="date">2019</span>
+          <p>Inspired by Bridget Riley's op art and the wallpapers in the confessionals of recent seasons of RuPaul's Drag Race, <em>Gradient Resonance</em> is an interactive op art piece. Move your cursor over or tap on the canvas to create a linear gradient from two full-saturation colours.</p>
+          <p>The resulting gradient is re-imposed on top of itself in a pattern. This creates an effect of different squares fading in from the top and bottom of the piece. The prominence of the effect will vary depending on the colours chosen for the gradient, and some interesting combinations have been highlighted on the piece.</p>
+          <p>
+            themes???
+          </p>
+        </div>
       </div>
     </div>
 
@@ -49,16 +52,18 @@
         <img src="./assets/arches-photo.jpg">
       </div>
       <div class="right">
-        <h2>Arches at Night in Leyland</h2>
-        <span class="date">2019</span>
-        <p>
-          colour
-          'capitalism' looming over
-          light pollution
-          urban density
+        <div class="right-inner">
+          <h2>Arches at Night in Leyland</h2>
+          <span class="date">2019</span>
+          <p>
+            colour
+            'capitalism' looming over
+            light pollution
+            urban density
 
-          need to crop these
-        </p>
+            need to crop these
+          </p>
+        </div>
       </div>
     </div>
 
@@ -67,11 +72,13 @@
         <img src="./assets/monalisa-photo.png">
       </div>
       <div class="right">
-        <h2>Mona Lisa in Rectanges</h2>
-        <span class="date">2019</span>
-        <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
-        <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
-        <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
+        <div class="right-inner">
+          <h2>Mona Lisa in Rectanges</h2>
+          <span class="date">2019</span>
+          <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
+          <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
+          <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
+        </div>
       </div>
     </div>
 
@@ -80,11 +87,13 @@
         <looking-for-work></looking-for-work>
       </div>
       <div class="right">
-        <h2>Looking for work</h2>
-        <span class="date">2019</span>
-        <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
-        <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
-        <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
+        <div class="right-inner">
+          <h2>Looking for work</h2>
+          <span class="date">2019</span>
+          <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
+          <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
+          <p>Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things. Here i will say some things.</p>
+        </div>
       </div>
     </div>
   </div>
@@ -149,8 +158,13 @@ export default {
   padding: 0.5rem;
 }
 
-.right > * {
+.right-inner {
   width: 90%;
+  max-height: 100%;
+  column-width: 15rem;
+}
+
+.right-inner > * {
   margin: 0;
   padding: 0.5rem;
 }
@@ -215,6 +229,11 @@ export default {
 
   .right {
     flex-wrap: nowrap;
+  }
+
+  .right-inner {
+    max-height: unset;
+    column-width: unset;
   }
 }
 </style>
